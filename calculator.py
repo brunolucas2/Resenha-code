e0b899998b2f0f8bdefa432613184937c48c2cfd
Bruno Lucas <161754446+brunolucas2@users.noreply.github.com>
2026-06-12 03:41:17 -0300

    Calculadora refatorada e atualizada

--- calculator.py
+++ calculator.py
@@ -1,40 +1,87 @@
 from os import system
 from time import sleep
 system('cls')
-def init():
-    n1 = float(input("Infomre o primeiro número: "))
-    n2 = float(input("Informe o segundo número: "))
-    result = 0.0
+
+def clearConsole():
     system('cls')
-    while True:
-        option = input(f"1.Soma\n2.Subtrair\n3.Multiplicar\n4.Dividir\n5.Resto de divisão\n6.Sair\n\nNúmeros:\nPrimeiro: {n1}\nSegundo: {n2}\n\nOpção escolhida: ")
-        system('cls')
-        print("Calculando o Resultado .....")
-        sleep(1)
+
+def calculator(option, n1, n2):
+    package = {
+        "operacao": "",
+        "resultado": 0
+    }
+    try:
         match option:
             case "1":
-                result = n1 + n2
-                break
+                package["operacao"], package["resultado"] = "soma", n1 + n2
             case "2":
-                result = n1 - n2
-                break
+                package["operacao"], package["resultado"] = "subtracao", n1 - n2
             case "3":
-                result = n1 * n2
-                break
+                package["operacao"], package["resultado"] = "multiplicar", n1 * n2
             case "4":
-                result = n1 / n2
-                break
+                package["operacao"], package["resultado"] = "dividir", n1 / n2
             case "5":
-                result = n1 % n2
-                break
-            case "6":
-                system('cls')
-                print("Adeus")
-                break
-    
-    system('cls')
-    if result:
-        print(result)
+                package["operacao"], package["resultado"] = "resto de divisao", n1 % n2
+            case _:
+                print("Essa opção não existe\nPofavor tentar novamente")
+                return "NotExistsThisOption"
+    except ZeroDivisionError:
+        package["operacao"], package["resultado"] = "erro", "divisao por zero"
+    except Exception as e:
+        package["operacao"], package["resultado"] = "erro", str(e)
 
+    return package
+
+def init():
+    clearConsole()
+    while True:
+
+        try:
+            firstNumber = float(input("Informe o primeiro número: "))
+            secondNumber = float(input("Informe o segundo número: "))
+        except ValueError:
+            print("Pofavor insira apenas Números")
+            continue
+
+        clearConsole()
+
+        option = input(f"""
+1.Soma
+2.Subtrair
+3.Multiplicar
+4.Dividir
+5.Resto de divisão
+6.Sair\n
+Números inseridos:\nPrimeiro: {firstNumber}\nSegundo: {secondNumber}\n
+Insira a opção: """)
+        
+        clearConsole()
+
+        if option == "6":
+            print("Adeus ...")
+            sleep(1.5)
+            break
+        else:
+            print("Calculando o Resultado .....")
+            resultado = calculator(option, firstNumber, secondNumber)
+            if not resultado:
+                print("ERRO interno!\nnão foi possivel realizar a operação")
+                continue
+            elif resultado == "NotExistsThisOption":
+                continue
+
+        sleep(1)
+        clearConsole()
+
+        input(f"""
+========================================================
+    Números inseridos: [ 1 = {firstNumber:.2f} / 2 = {secondNumber:.2f} ]
+    Operação Selecionada: ( {resultado["operacao"]} )
+    Resultado: {resultado["resultado"]:.2f}
+
+    pra voltar ao menu pressione qualquer botão ...
+========================================================
+            """)
+        
 if __name__=="__main__":
     init()
